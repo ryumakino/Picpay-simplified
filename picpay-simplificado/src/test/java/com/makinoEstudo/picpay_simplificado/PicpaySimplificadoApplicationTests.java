707b4c22c9a6863c86b7edff11a22cccbfd5aa42
ryu <ricardoryu.makino@gmail.com>
2025-08-04 12:00:29 -0300

    Initial commit

---
+++ picpay-simplificado/src/test/java/com/makinoEstudo/picpay_simplificado/PicpaySimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makinoEstudo.picpay_simplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaySimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
